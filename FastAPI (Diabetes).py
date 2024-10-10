bf03c51a5cb9bad5717fe1a550bd12be53e2e44b
huynhquangquan <150917510+huynhquangquan@users.noreply.github.com>
2024-10-10 17:25:34 +0700

    Add files via upload

---
+++ FastAPI (Diabetes).py
@@ -0,0 +1,57 @@
+# 1. Library imports
+import uvicorn
+from fastapi import FastAPI, HTTPException
+import numpy as np
+import joblib
+import pandas as pd
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware # Post request for HTML
+
+# 	Pregnancies	Glucose	BloodPressure	SkinThickness	Insulin	BMI	DiabetesPedigreeFunction	Age
+class Diabetes(BaseModel):
+    Pregnancies: int
+    Glucose: int
+    BloodPressure: int
+    SkinThickness: int
+    Insulin: float
+    BMI: float
+    DiabetesPedigreeFunction: float
+    Age: int
+
+app = FastAPI()
+
+# CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # You can specify allowed origins instead of '*' for better security
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allows all headers
+)
+
+full_pipeline = joblib.load("random_forest_optimized_diabetes.pkl")
+
+@app.post('/predict/')
+async def predict_adclick(input_data: Diabetes):
+    try:
+        x_values = pd.DataFrame({
+            'Pregnancies': [int(input_data.Pregnancies)],
+            'Glucose': [int(input_data.Glucose)],
+            'BloodPressure': [int(input_data.BloodPressure)],
+            'SkinThickness': [int(input_data.SkinThickness)],
+            'Insulin': [int(input_data.Insulin)],
+            'BMI': [float(input_data.BMI)],
+            'DiabetesPedigreeFunction': [float(input_data.DiabetesPedigreeFunction)],
+            'Age': [int(input_data.Age)]
+        })
+
+        prediction = full_pipeline.predict(x_values)
+
+        prediction_result = 'Diabetes' if prediction[0] == 1 else 'Not Diabetes'
+        return {'prediction': prediction_result}
+
+    except Exception as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+if __name__ == '__main__':
+    uvicorn.run(app, host='127.0.0.1', port=8000)
