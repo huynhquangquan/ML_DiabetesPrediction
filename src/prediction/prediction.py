4a76645461f6ead760f322f07e83619d4c1080c3
huynhquangquan <150917510+huynhquangquan@users.noreply.github.com>
2024-11-13 19:40:20 +0700

    Update prediction.py

--- src/prediction/prediction.py
+++ src/prediction/prediction.py
@@ -11,28 +11,27 @@ if __name__=="__main__":
     scaling = utilities.scaling_config()
     check_raw = bool(utilities.check_raw(dataset))
     check_processed = bool(utilities.check_processed())
-    check_external = bool(utilities.check_external())
     check_model = bool(utilities.check_model(model_name))
-    if check_raw is False or check_model is False or check_processed is False or check_external is False:
+    if check_raw is False or check_model is False or check_processed is False:
         raise RuntimeError("Prediction thất bại")
 
     # Load data-samples data and model
     model = utilities.joblib_load(model_name)
     data_samples_processed = utilities.read_processed(dataset)
     data_samples_raw = utilities.read_raw(dataset)
-    data_sample_external = utilities.read_processed('test.csv')
-    # data_sample_external = utilities.read_external()
+    data_test = utilities.read_processed('test.csv')
+    # data_test = utilities.read_test()
     # Shuffle before defining X and y
     data_samples_processed = data_samples_processed.sample(frac=1)
-    data_samples_external = data_sample_external.sample(frac=1)
+    data_test = data_test.sample(frac=1)
     data_samples_raw = data_samples_raw.sample(frac=1)
 
     X_processed = data_samples_processed.drop(columns=["Outcome"])
     y_processed = data_samples_processed["Outcome"]
     X_raw = data_samples_raw.drop(columns=["Outcome"])
     y_raw = data_samples_raw["Outcome"]
-    X_external = data_samples_external.drop(columns=["Outcome"])
-    y_external = data_samples_external["Outcome"]
+    X_test = data_test.drop(columns=["Outcome"])
+    y_test = data_test["Outcome"]
 
     threshold = float(utilities.model_select()['threshold'])
     if threshold > 1 or threshold <= 0:
@@ -43,26 +42,26 @@ if __name__=="__main__":
         X_train = X_train.drop(columns=['Outcome'])
         bin, X_processed_scaled = utilities.scaling(X_train,X_processed)
         bin, X_raw_scaled = utilities.scaling(X_train,X_raw)
-        bin, X_external_scaled = utilities.scaling(X_train,X_external)
+        bin, X_test_scaled = utilities.scaling(X_train,X_test)
         X_train, bin = None, None
     else:
         X_processed_scaled = X_processed
         X_raw_scaled = X_raw
-        X_external_scaled = X_external
+        X_test_scaled = X_test
 
     y_proba = model.predict_proba(X_processed_scaled)[:, 1]
     pred_processed = (y_proba >= threshold).astype(int)
     y_proba = model.predict_proba(X_raw_scaled)[:, 1]
     pred_raw = (y_proba >= threshold).astype(int)
-    y_proba = model.predict_proba(X_external_scaled)[:, 1]
-    pred_external = (y_proba >= threshold).astype(int)
+    y_proba = model.predict_proba(X_test_scaled)[:, 1]
+    pred_test = (y_proba >= threshold).astype(int)
 
     X_processed['Actual'] = y_processed
     X_processed['Predict'] = pred_processed
     X_raw['Actual'] = y_raw
     X_raw['Predict'] = pred_raw
-    X_external['Actual'] = y_external
-    X_external['Predict'] = pred_external
+    X_test['Actual'] = y_test
+    X_test['Predict'] = pred_test
 
     # Print and export results of prediction
     # PROCESSED
@@ -85,13 +84,13 @@ if __name__=="__main__":
     print(f'Accuracy on Not Diabetes: {accuracy_on_neg_r*100:.2f}%')
     print(f'Accuracy on Diabetes: {accuracy_on_pos_r*100:.2f}%')
 
-    # EXTERNAL
-    print("Pred vs External Dataset")
-    print(X_external)
-    accuracy_external = (X_external['Actual']==X_external['Predict']).sum() / len(X_external)
-    accuracy_on_neg_xtn = ((X_external['Actual']==0) & (X_external['Predict']==0)).sum()  / (X_external['Actual']==0).sum()
-    accuracy_on_pos_xtn = ((X_external['Actual']==1) & (X_external['Predict']==1)).sum() / (X_external['Actual']==1).sum()
-    print(f'Accuracy: {accuracy_external*100:.2f}%')
+    # TEST
+    print("Preview: Pred vs Test Dataset")
+    print(X_test)
+    accuracy_test = (X_test['Actual']==X_test['Predict']).sum() / len(X_test)
+    accuracy_on_neg_xtn = ((X_test['Actual']==0) & (X_test['Predict']==0)).sum()  / (X_test['Actual']==0).sum()
+    accuracy_on_pos_xtn = ((X_test['Actual']==1) & (X_test['Predict']==1)).sum() / (X_test['Actual']==1).sum()
+    print(f'Accuracy: {accuracy_test*100:.2f}%')
     print(f'Accuracy on Not Diabetes: {accuracy_on_neg_xtn*100:.2f}%')
     print(f'Accuracy on Diabetes: {accuracy_on_pos_xtn*100:.2f}%')
 
@@ -107,8 +106,8 @@ if __name__=="__main__":
             f'{accuracy_on_neg_r * 100:.2f}%',
             f'{accuracy_on_pos_r * 100:.2f}%'
         ],
-        "External": [
-            f'{accuracy_external * 100:.2f}%',
+        "Preview Test": [
+            f'{accuracy_test * 100:.2f}%',
             f'{accuracy_on_neg_xtn * 100:.2f}%',
             f'{accuracy_on_pos_xtn * 100:.2f}%'
         ]
